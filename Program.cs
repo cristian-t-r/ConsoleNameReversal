65860ac392259c96c457175437dc994ab881feb8
cristian-t-ramirez <wavedasher@protonmail.ch>
2021-10-04 10:01:21 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleNameReversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string lastName;
+            string fullName;
+            string reversedName;
+            string charCount;
+
+            Console.WriteLine("What is your first name?\n");
+            firstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?\n");
+            lastName = Console.ReadLine();
+
+            fullName = firstName + " " + lastName;
+            charCount = fullName.Length.ToString();
+            reversedName = NameReverse(fullName);
+
+            Console.WriteLine("Your reversed name is " + reversedName + "\n");
+            Console.WriteLine("Your name is " + charCount + "characters long.\n");
+
+        }
+        private static string NameReverse(string name)
+        {
+            char[] charArray = name.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
